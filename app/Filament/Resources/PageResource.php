25eefc6d400e007256f73eace9665eb962c53308
Jean Luc Houedanou <jeanluc@houedanou.com>
2025-06-26 19:27:33 +0000

    feat: Ajouter la page de création pour la ressource Page

--- app/Filament/Resources/PageResource.php
+++ app/Filament/Resources/PageResource.php
@@ -160,6 +160,7 @@ class PageResource extends Resource
     {
         return [
             'index' => Pages\ListPages::route('/'),
+            'create' => Pages\CreatePage::route('/create'),
             'view' => Pages\ViewPage::route('/{record}'),
             'edit' => Pages\EditPage::route('/{record}/edit'),
         ];
