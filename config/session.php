043a0a55dc54925f3efbe793eda75cec6010fa59
Jean Luc Houedanou <jeanluc@houedanou.com>
2025-06-30 16:33:44 +0000

    645

--- config/session.php
+++ config/session.php
@@ -59,7 +59,7 @@ return [
     |
     */
 
-    'files' => storage_path('framework/sessions'),
+    'files' => env('SESSION_PATH', storage_path('framework/sessions')),
 
     /*
     |--------------------------------------------------------------------------
